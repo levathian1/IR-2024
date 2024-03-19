174315e814b90fd1a84e8720f383f61f9781c773
levathian1 <randomynot02@gmail.com>
2024-03-19 15:12:10 +0100

    plotter update with speed calc

--- plotter.py
+++ plotter.py
@@ -30,6 +30,11 @@ gmap.draw("my_map.html")
 
 print(geodesic((48.672945486, 6.153757156), (48.666823756, 6.145844497)).kilometers)
 
+# TODO: find stackoverflow where this was found to credit it
+
+# https://stackoverflow.com/questions/72823034/pandas-calculating-the-speed-between-two-rows-based-on-timestamp-and-coordinates
+# lambda function 
+
 print(df.head())
 # Calculate distance and time difference
 df['prev_latitude'] = df['latitude(deg)'].shift(-1)
@@ -38,10 +43,8 @@ if(pd.isna(df['prev_latitude'].shift(-1).any()) == False):
     df['distance'] = df.apply(lambda x: haversine((x['latitude(deg)'], x['longitude(deg)']), (x['prev_latitude'], x['prev_longitude'])), axis=1)
     df['time_diff'] = (df['Time'] - df['Time'].shift()).dt.total_seconds() / 3600  # Convert seconds to hours
 
-# Calculate speed and add a new column to the DataFrame
 df['speed'] = df['distance'] / df['time_diff']
 
-# Drop intermediate columns (optional)
 df = df.drop(['distance', 'time_diff'], axis=1)
 
-print(df.head())
+print(df)
