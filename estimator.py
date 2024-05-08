7cfb14cdf081274fab6d1b3525be1e934bb1003b
levathian1 <randomynot02@gmail.com>
2024-05-08 21:09:19 +0200

    km estimation

--- estimator.py
+++ estimator.py
@@ -56,19 +56,24 @@ def calc_accel():
     print("avg accel", accel_moy)
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iterrows.html#pandas-dataframe-iterrows
     accel = list()
+    angle = list()
+    speed = list()
     for i in range(0, df.shape[0]-1):
         accel.append((df["speed"].iloc[i] - df["speed"].iloc[i+1]) / 0.2)
-        print('accel[i]')
+        # print('accel[i]')
 
     # print(accel)
     km = list()
     for i in range(len(accel)):
-        f_a = ( air_density / 2 ) * cx * frontal_area * df['intensity'].iloc[i] ** 2
-        f_r = cr * mass *  g * math.sin(deg_to_rad(df['angle'].iloc[i]))
-        f_p = mass * g * math.sin(deg_to_rad(df['angle'].iloc[i]))
-        km.append((0.2 / (df['intensity'].iloc[i] ) * (mass * accel[i]) - f_a - f_r - f_p))
+        if(df['intensity'].iloc[i] > max_int):
+            # print(df['intensity'].iloc[i])
+            f_a = ( air_density / 2 ) * cx * frontal_area * df['intensity'].iloc[i] ** 2
+            f_r = cr * mass *  g * math.sin(deg_to_rad(df['angle'].iloc[i]))
+            f_p = mass * g * math.sin(deg_to_rad(df['angle'].iloc[i]))
+            # print((0.2 / (df['intensity'].iloc[i] ) * (mass * accel[i]) - f_a - f_r - f_p))
+            km.append((0.5  * (mass * accel[i]) - f_a - f_r - f_p)/ (df['intensity'].iloc[i]  ))
 
-    print(df.shape[0]-1)
+    # print(df.shape[0]-1)
     std_dev = np.std(np.array(km))
     km_nb = np.mean(np.array(km))
     print('dev: ', std_dev)
@@ -78,29 +83,41 @@ def calc_accel():
             # k_m = 114 / mass
             # https://www.renault.fr/achat-vehicules-neufs/vehicules.html?productId=VEH_VF1RFK00870363236
             # kangoo engine traction
+            angle.append(float(row['angle']))
+            speed.append(float(row['speed']))
             k_m = 245 / row['intensity']
-            f_m = ( k_m / 0.2032 ) * row['intensity'] # 18 inch tire
+            f_m = ( km_nb / 0.5 ) * row['intensity'] # 18 inch tire
             f_f = 0 # calc on speed > 0
             f_a = ( air_density / 2 ) * cx * frontal_area * row['speed'] ** 2
             f_r = cr * mass *  g * math.sin(deg_to_rad(row['angle']))
             f_p = mass * g * math.sin(deg_to_rad(row['angle']))
             a = (f_m - f_f - f_a - f_r - f_p) / mass
             res.append(a)
-            fin_arr.append([k_m, f_m, f_f, f_a, f_r, f_p])
+            fin_arr.append([km_nb, f_m, f_f, f_a, f_r, f_p])
             # print(k_m)
 
 
             
     # -ma = k * v**2 + f_rl * m * g + m * g * sin theta - F
-    return res, fin_arr, accel
+    return res, fin_arr, accel, angle, speed
 
 # https://stackoverflow.com/questions/4440516/in-python-is-there-an-elegant-way-to-print-a-list-in-a-custom-format-without-ex
-val, arr, acc = calc_accel()
+val, arr, acc, ang, sp = calc_accel()
 # print('\n'.join('{}: {}'.format(*k) for k in enumerate(val)))
 plt.figure()
 plt.plot(df['Time'][0:15], val[0:15])
 # plt.legend()
 plt.title(f"Acceleration au cours du temps")
+
+print(sp)
+
+plt.figure()
+plt.plot(sp, ang)
+plt.title(f"Variation de la pente en fonction de la vitesse")
+
+plt.figure()
+plt.plot(df['Time'][:335], ang)
+plt.title(f"Variation de la pente au cours du temps")
 
 
 # print(len(arr)/2)
@@ -119,9 +136,9 @@ res2 = clf.predict(arr_p)
 
 print("len: ", len(res2), len(val[int(len(arr)/2)+1:]))
 
-print(res2)
-print(val[int(len(arr)/2)+1:])
-# print(val)
+# print(res2)
+# print(val[int(len(arr)/2)+1:])
+# # print(val)
 
 for i in range(0, int(len(arr)/2)):
     print("gap", res2[i] - val[i + int(len(arr)/2)+1])
